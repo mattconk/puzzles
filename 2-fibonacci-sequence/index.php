b5e86576cefcc82a86cc0e79377658b64b6e72c9
Matthew Conk <mattconk@gmail.com>
2014-04-07 14:59:38 -0500

    updating function

--- 2-fibonacci-sequence/index.php
+++ 2-fibonacci-sequence/index.php
@@ -13,6 +13,18 @@ $formating_arrays = new Formating_Arrays();
 
 $puzzle_name = '2-fibonacci-sequence';
 
+function sequence($n)
+{
+    if ($n == 1 || $n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return sequence($n - 1) + sequence($n - 2);
+    }
+}
+
 
 echo $formating_html->page_open($puzzle_name);
 $puzzle = '<h3>Puzzle:</h3><p>By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.</p>'; 
@@ -21,7 +33,18 @@ $puzzle = '<h3>Puzzle:</h3><p>By considering the terms in the Fibonacci sequence
     echo '<a href="http://projecteuler.net/problem=2" target="_blank">Puzzles Page</a>';
     echo '<br><hr>';
     
+    $i = 1;
+    $result = $i;
+    do 
+    {
+        echo $result.', ';
+        $result = sequence($i);
+        $i++;
+        
+    } while ($result < 4000000);
+    
 
+    
     echo '<br>';
 
 echo '</body>';
