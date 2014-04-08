94e998accf11686abe94f34df0e02deaf45e0d0d
Matthew Conk <mattconk@gmail.com>
2014-04-08 08:44:44 -0500

    finished fibonacci sequence

--- 2-fibonacci-sequence/index.php
+++ 2-fibonacci-sequence/index.php
@@ -13,6 +13,13 @@ $formating_arrays = new Formating_Arrays();
 
 $puzzle_name = '2-fibonacci-sequence';
 
+/**
+ * generates a Fibonacci Squence number using recursive finctions
+ * 
+ * @param int $n The value to find the Fibonacci Sequence value for
+ * @return int The next number in the Fibonacci Sequence.
+ * @todo This function seems, so I might need to look into streamlining it.
+ */
 function sequence($n)
 {
     if ($n == 1 || $n == 2)
@@ -35,17 +42,22 @@ $puzzle = '<h3>Puzzle:</h3><p>By considering the terms in the Fibonacci sequence
     
     $i = 1;
     $result = $i;
+    $sum = 0;
     do 
     {
-        echo $result.', ';
+
+        if ($result % 2 == 0)
+        {
+            $sum += $result;
+        }
         $result = sequence($i);
         $i++;
         
     } while ($result < 4000000);
     
-
+    echo '<br>'.$sum;
     
-    echo '<br>';
+    echo '<br><script src="https://gist.github.com/mattconk/2ae3d03b354a2fd2cb5c.js"></script>';
 
 echo '</body>';
 	
