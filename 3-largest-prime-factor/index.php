7fb4395b09df69a0b1780dd4f6e1e744ab2a88f3
Matthew Conk <mattconk@gmail.com>
2014-04-08 13:11:13 -0500

    updates to problem

--- 3-largest-prime-factor/index.php
+++ 3-largest-prime-factor/index.php
@@ -21,17 +21,35 @@ function primeFactor($n)
     }
     else 
     {
-        for($i=2; $i<$n; $i++)
+        for ($i=2; $i<=$n/2; $i++)
         {
-            if ( $n % $i == 0 )
+            if ($n % $i == 0 && isPrime($i) == TRUE)
             {
-                echo $i.', ';
-            }    
+                echo $i.'<br>';
+                $largest = $i;
+            }
         }
-        
-            return $i;
+        return $largest;
     }
 
+}
+
+/**
+ * Determines if a number is a Prime
+ * 
+ * @param int $n The number to consider
+ * @return boolean
+ */
+function isPrime($n)
+{
+    for ($i=2; $i*$i<=$n; $i++)
+    {
+        if ($n % $i == 0)
+        {
+            return FALSE;
+        }
+    }
+    return TRUE;
 }
 
 echo $formating_html->page_open($puzzle_name);
@@ -40,8 +58,22 @@ $puzzle = '<h3>Puzzle:</h3><p>What is the largest prime factor of the number 600
     echo $puzzle;
     echo '<a href="http://projecteuler.net/problem=3" target="_blank">Puzzles Page</a>';
     echo '<br><hr>';
+
+
     
     $n = 13195;
+//     $n = 600851475143;
+
+
+//     for ($i=2; $i<=$n/2; $i++)
+//     {
+//         if($n % $i == 0)
+//         {
+//             echo $i.', ';
+//         }
+//     }
+//     exit;
+    
     echo 'The largest prime factor of '.$n.' is '.primeFactor($n);
     
     echo '<br>';
