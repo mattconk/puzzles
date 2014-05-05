3aff8df3076a92bf7a4c304dc11658f89d7ee2eb
Matthew Conk <mattconk@gmail.com>
2014-05-04 19:03:45 -0500

    removed test function

--- classes/formating_arrays.php
+++ classes/formating_arrays.php
@@ -46,16 +46,6 @@ class Formating_Arrays {
         
         return $title.' = ( '.implode(', ',$numbers).' )';
     }
-    
-    /**
-     * test function to ensure I have git and eclipse set up correctly.
-     * 
-     * @return boolean
-     */
-    function testing()
-    {
-    	//A basic function to make sure I git working correctly in linux
-    	return FALSE;
-    }
+ 
 }
 
